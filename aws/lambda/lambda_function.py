97bea26f8316470d34a675dbc63e04d4d84802e3
0xThresh.eth <0xthresh@protonmail.com>
2023-11-24 16:34:55 -0800

    Updated Lambda function

--- aws/lambda/lambda_function.py
+++ aws/lambda/lambda_function.py
@@ -1,56 +1,62 @@
-import boto3
 import json
+import os
+import requests
+import datetime
+import logging
 
 def lambda_handler(event, context):
-    # Extract the crypto pair values from the Amazon Lex event
-    # Assuming the slots are named 'cryptoBase' and 'cryptoTarget'
+    logger = logging.getLogger()
+    # Extract slots from Lex event
     crypto_base = event['currentIntent']['slots']['cryptoBase']
     crypto_target = event['currentIntent']['slots']['cryptoTarget']
     
-    # Form the pair string, e.g., "ETH-USD"
-    crypto_pair = f"{crypto_base}-{crypto_target}"
+    # Check if both cryptoBase and cryptoTarget are provided
+    if not crypto_base or not crypto_target:
+        return close(event, 'Fulfilled', 'Please provide both base and target cryptocurrencies.')
 
+    # Prepare API query
+    task_ip = "10.0.0.7"
+    contract_address = "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419"
+    current_timestamp = int(datetime.datetime.now().timestamp())
+    api_key = os.getenv('API_KEY')  # Retrieve API key from Lambda environment variable
+    chain = "mainnet"
+    api_endpoint = f"http://{task_ip}:3000/api/price?contractAddress={contract_address}&startTimestamp={current_timestamp}&endTimestamp={current_timestamp}&chain={chain}&rpcUrl={api_key}"
+
+    # Make the API request
     try:
-        # Placeholder for smart contract interaction to get the price
-        price = get_crypto_price_from_smart_contract(crypto_pair)
-
-        # Formulate the response message
-        message = f"The current price of {crypto_base} to {crypto_target} is {price}."
-        fulfillment_state = "Fulfilled"
-
+        response = requests.get(api_endpoint)
+        response_data = response.json()
     except Exception as e:
-        # Handle any exceptions that occur during the smart contract call
-        message = "Sorry, there was an error getting the price."
-        fulfillment_state = "Failed"
+        return close(event, 'Fulfilled', f"Error making API request: {str(e)}")
 
-    # Create the response structure for Amazon Lex
+    # Parse the response
+    try:
+        logger.info("FULL RESPONSE DATA")
+        logger.info(response_data)
+        initial_price = int(response_data['rounds'][-1]['answer'])
+        logger.info("INITIAL PRICE")
+        logger.info(initial_price)
+        decimals = int(response_data['decimals'])
+        logger.info("DECIMALS")
+        logger.info(decimals)
+        latest_price = initial_price / (10 ** decimals)
+        message = f"The latest price for {crypto_base}/{crypto_target} is: {latest_price}"
+    except (IndexError, KeyError):
+        message = "Could not parse the price information from the response."
+        logger.error("Failed to parse price info from user response")
+
+    return close(event, 'Fulfilled', message)
+
+def close(event, fulfillment_state, message):
     response = {
-        "dialogAction": {
-            "type": "Close",
-            "fulfillmentState": fulfillment_state,
-            "message": {
-                "contentType": "PlainText",
-                "content": message
+        'sessionAttributes': event['sessionAttributes'],
+        'dialogAction': {
+            'type': 'Close',
+            'fulfillmentState': fulfillment_state,
+            'message': {
+                'contentType': 'PlainText',
+                'content': message
             }
         }
     }
-
     return response
-
-def get_crypto_price_from_smart_contract(crypto_pair):
-    # This function should interact with your smart contract and return the price
-    # Replace this with your actual smart contract interaction code
-    raise NotImplementedError("Smart contract interaction not implemented.")
-
-# Test the Lambda function with a simulated Lex event
-test_event = {
-    "currentIntent": {
-        "slots": {
-            "cryptoBase": "ETH",
-            "cryptoTarget": "USD"
-        }
-    }
-}
-
-# Uncomment the following line to test the function once you have implemented the smart contract interaction
-# lambda_handler(test_event, None)
